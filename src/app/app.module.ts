da0017293f69e1582208b6beeab6dee282d39304
markodojkic <marko.dojkic@gmail.com>
2021-04-01 19:51:33 +0200

    Updating registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RoutingModule } from './routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
+import { MaterialModule } from './material.module';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { RegistrationComponent } from './auth/registration/registration.componen
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    MaterialModule,
+    RoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
