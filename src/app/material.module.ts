a8d01cc79d7e89a2cc29b092c8ce7ffc7438763b
markodojkic <marko.dojkic@gmail.com>
2021-04-02 17:47:53 +0200

    Finished login and registration page frontend, added toolbar and implemented custom theme

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -10,13 +15,23 @@ import { MatInputModule } from '@angular/material/input';
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatCheckboxModule,
+        MatSlideToggleModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatCheckboxModule,
+        MatSlideToggleModule
     ]
 })
 
